7c0906fdefb9d150aae8139cfe9e9724b345155f
Полина Артамонова <poliiiiinka71@gmail.com>
2024-12-28 23:52:59 +0300

    добавлен слайдер

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,40 +1,190 @@
-﻿alert("я подключился??")
+﻿alert("Раздел \"О проекте\"")
 
-/*
-* Запросим пользовательский ввод
-* и сохраним отзыв в объект
-*
-* */
-function getReview() {
-    // Создадим объект
-    let review = {}
-
-    // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
+// Конструктор, через который создаётся комментарий
+function Comment() {
+    // Запросим имя
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
 
-    // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
+    // Запросим текст
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
         return
     }
 
     // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-
-    // Добавим на страницу
-    writeReview(review)
+    this.date = new Date().toLocaleString()
 }
 
-/*
-* Запишем отзыв на страницу
-*
-* */
+// Оставить комментарий
+function addComment() {
+    let comment = new Comment()
+
+    // проверяем, успешно ли юзер осуществил ввод
+    if (comment.empty) {
+        return;
+    }
+
+    // Запросим, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment)
+        // и добавим ему нужное свойство
+        review.rate = 0;
+
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review)
+    } else {
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment)
+    }
+}
+
 const writeReview = review => {
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>` +
-        '</div>';
+    let likeCounter = '';
+
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
+    if (review.hasOwnProperty('rate')) {
+
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-text">\n' +
+        `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
 }
+
+// Увеличивает счётчик лайков
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
+}
+
+let slider = document.querySelector('.slider'),
+    sliderList = slider.querySelector('.slider-list'),
+    sliderTrack = slider.querySelector('.slider-track'),
+    slides = slider.querySelectorAll('.slide'),
+    arrows = slider.querySelector('.slider-arrows'),
+    prev = arrows.children[0],
+    next = arrows.children[1],
+    slideWidth = slides[0].offsetWidth,
+    slideIndex = 0,
+    posInit = 0,
+    posX1 = 0,
+    posX2 = 0,
+    posFinal = 0,
+    posThreshold = slideWidth * .35,
+    trfRegExp = /[-0-9.]+(?=px)/,
+    slide = function () {
+        sliderTrack.style.transition = 'transform .5s';
+        sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
+
+        // делаем стрелку prev недоступной на первом слайде
+        // и доступной в остальных случаях
+        // делаем стрелку next недоступной на последнем слайде
+        // и доступной в остальных случаях
+        prev.classList.toggle('disabled', slideIndex === 0);
+        next.classList.toggle('disabled', slideIndex === --slides.length);
+    }
+
+
+getEvent = () => event.type.search('touch') !== -1 ? event.touches[0] : event,
+
+    swipeStart = function () {
+        let evt = getEvent();
+
+        // берем начальную позицию курсора по оси Х
+        posInit = posX1 = evt.clientX;
+
+        // убираем плавный переход, чтобы track двигался за курсором без задержки
+        // т.к. он будет включается в функции slide()
+        sliderTrack.style.transition = '';
+
+        // и сразу начинаем отслеживать другие события на документе
+        document.addEventListener('touchmove', swipeAction);
+        document.addEventListener('touchend', swipeEnd);
+        document.addEventListener('mousemove', swipeAction);
+        document.addEventListener('mouseup', swipeEnd);
+    },
+    swipeAction = function () {
+        let evt = getEvent(),
+            // для более красивой записи возьмем в переменную текущее свойство transform
+            style = sliderTrack.style.transform,
+            // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
+            transform = +style.match(trfRegExp)[0];
+
+        posX2 = posX1 - evt.clientX;
+        posX1 = evt.clientX;
+
+        sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
+    }
+
+swipeEnd = function () {
+    // финальная позиция курсора
+    posFinal = posInit - posX1;
+
+    document.removeEventListener('touchmove', swipeAction);
+    document.removeEventListener('mousemove', swipeAction);
+    document.removeEventListener('touchend', swipeEnd);
+    document.removeEventListener('mouseup', swipeEnd);
+
+    // убираем знак минус и сравниваем с порогом сдвига слайда
+    if (Math.abs(posFinal) > posThreshold) {
+        // если мы тянули вправо, то уменьшаем номер текущего слайда
+        if (posInit < posX1) {
+            slideIndex--;
+            // если мы тянули влево, то увеличиваем номер текущего слайда
+        } else if (posInit > posX1) {
+            slideIndex++;
+        }
+    }
+
+    // если курсор двигался, то запускаем функцию переключения слайдов
+    if (posInit !== posX1) {
+        slide();
+    }
+};
+
+arrows.addEventListener('click', function () {
+    let target = event.target;
+
+    if (target === next) {
+        slideIndex++;
+    } else if (target === prev) {
+        slideIndex--;
+    } else {
+        return;
+    }
+
+    slide();
+});
+
+sliderTrack.style.transform = 'translate3d(0px, 0px, 0px)';
+
+slider.addEventListener('touchstart', swipeStart);
+slider.addEventListener('mousedown', swipeStart);
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,21 +1,18 @@
-﻿// Сессия теперь создается в общей области видимости
-// Будет "захватываться" тремя функциями
-let session = new Map();
-
-// Сохранение данных сессии сразу при заходе пользователя на страницу
-function handleSession() {
-    // Сохраним время начала сессии
-    session.set("startDate", new Date().toLocaleString())
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
+﻿// Сохранение данных сессии сразу при заходе пользователя на страницу
+let session = {
+    'startDate': new Date().toLocaleString(),
+    'userAgent': window.navigator.userAgent,
+    'userAge': prompt("Пожалуйста, введите ваш возраст?")
 }
 
 // Проверка возраста пользователя
-function checkAge() {
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-
-    if (session.get("age") >= 18) {
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
@@ -24,22 +21,54 @@ function checkAge() {
 }
 
 // Вывод данных сессии в консоль
-let sessionLog = function logSession() {
-    for (let result of session) {
-        console.log(result)
-    }
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
 }
 
 function filterContent() {
-
     let elements = document.getElementsByClassName('video-container');
 
     for (let i = 0; i <= elements.length; i++) {
-        let videoText = elements[i].querySelector(".video-title").innerText;
-        if (!videoText.toLowerCase().includes(inputParseFunction() /*Захват переменной происходит с помощью замыкания*/ .toLowerCase())) {
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
         }
     }
+}
+
+function handleSession(logger, checker) {
+
+    // Проверяем дату захода и проставляем, если новый визит
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+
+    // Проверяем userAgent и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+
+    // Проверяем возраст и проставляем, если новый визит
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    } else {
+
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
 }
